b2857e8e7f7dd00be32c71f6ad414e20dff0649c
AnxianZhang <gadanxianzhang@gmail.com>
2022-07-26 16:41:11 +0200

    a gradient for the life bar was added

--- 1rt-game/Assets/Script/HealthBar.cs
+++ 1rt-game/Assets/Script/HealthBar.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider healtBar;
+    public Slider healthBar;
+    public Gradient gradient;
+    public Image fill;
 
     public void initHealth(uint amount)
     {
-        healtBar.maxValue = amount;
-        healtBar.value = amount;
+        healthBar.maxValue = amount;
+        healthBar.value = amount;
+
+        fill.color = gradient.Evaluate(1f); // the value A [0, 1] <=> [0%, 100%] of a color asigned in the gradient component
     }
 
     public void setHealth(uint amount)
     {
-        healtBar.value = amount;
+        healthBar.value = amount;
+
+        fill.color = gradient.Evaluate(healthBar.normalizedValue);
     }
 }
